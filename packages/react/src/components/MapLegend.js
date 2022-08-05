5527edd1b3afcde7bd8da4c19f5d439d5bd22e16
Nick dos Remedios <nick.dosremedios@csiro.au>
2022-08-05 12:44:46 +1000

    Legend refinements

--- packages/react/src/components/MapLegend.js
+++ packages/react/src/components/MapLegend.js
@@ -15,14 +15,14 @@ function Legend({ legendState, setLegendState, coloursForCounts }) {
 
       legend.onAdd = () => {
         const div = L.DomUtil.create('div', 'info legend')
-        const title = '<h4>Records per square</h4>'
+        const title = '<h4>Records per grid</h4>'
         const entries = []
         const keysArray = Object.keys(coloursForCounts)
         keysArray.forEach((count, index) => {
           const lower = Number(keysArray[index - 1] || 0) + 1
-          const upper = keysArray.length === index + 1 ? `${count}+` : count
+          const upper = keysArray.length === index + 1 ? ` +` : `– ${count}`
           entries.push(
-            `<i style="background:${coloursForCounts[count]}"></i><span>${lower} – ${upper}</span><br>`
+            `<i style="background:${coloursForCounts[count]}"></i><span>${lower} ${upper}</span><br>`
           )
         })
         div.innerHTML = `${title}${entries.join('')}`
